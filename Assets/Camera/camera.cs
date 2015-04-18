1d7571c220d31ee4fa472085dab457b8189d0f5e
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2015-04-18 18:33:00 +0800

    Hopefully fixed the scene lol.

---
+++ Assets/Camera/camera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour 
+{
+    public GameObject m_playerKart;
+
+	// Use this for initialization
+	void Start () 
+    {
+         
+	}
+
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 kartPos = m_playerKart.transform.position;
+        kartPos.z -= 10.0f;
+        this.gameObject.transform.position = kartPos;
+    }
+}
