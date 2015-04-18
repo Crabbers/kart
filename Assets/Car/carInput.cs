ecc8b4f0eeeaee7861b41268f9cd5f2ef00f0966
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2015-04-18 16:57:16 +0800

    Initial cart test commit.

---
+++ Assets/Car/carInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carInput : MonoBehaviour {
+
+    public WheelCollider m_frontLeft;
+    public WheelCollider m_frontRight;
+    public WheelCollider m_backLeft;
+    public WheelCollider m_backRight;
+
+    private int m_speed = 1000;
+    private int m_breaking = 1;
+    private int m_turning = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(m_backRight.transform.position);
+        float verticalAxis = Input.GetAxis("Vertical") * m_speed;
+        float horizontalAxis = Input.GetAxis("Horizontal") * m_turning;
+        m_backLeft.motorTorque = m_backRight.motorTorque = verticalAxis;
+        m_frontLeft.steerAngle = m_frontRight.steerAngle = horizontalAxis;
+
+        if (Input.GetKey(KeyCode.Space)) {
+            m_backLeft.brakeTorque = m_backRight.brakeTorque = m_breaking;
+        }
+	}
+}
