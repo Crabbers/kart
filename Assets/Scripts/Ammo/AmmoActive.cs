ed992296b2e37fffc1860498cdc032d9b8008549
BobSmun <somemadeupname@gmail.com>
2015-04-19 18:03:10 +0800

    Ammo stuff

---
+++ Assets/Scripts/Ammo/AmmoActive.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoActive : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        AmmoStorage container = col.transform.GetComponentInChildren<AmmoStorage>();
+        if (container != null)
+        {
+            Destroy(this.gameObject);
+            //Explode?
+        }
+    }
+}
---
+++ Assets/Scripts/Ammo/AmmoStorage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoStorage : MonoBehaviour
+{
+    public int AmmoCount = 0;
+    public Transform ActiveAmmoPrefab;
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Jump")
+            && AmmoCount > 0)
+        {
+            AmmoCount--;
+
+            Transform p = (Transform)Instantiate(ActiveAmmoPrefab, transform.localPosition + new Vector3(2, 0, 0), transform.localRotation);
+        }
+    }
+
+    public void AddAmmo()
+    {
+        AmmoCount++;
+    }
+}
