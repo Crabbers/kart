6e1fb4f23dc5e770fe470120e8f5330ac99b1d00
jstennant <jono.tennant@gmail.com>
2015-04-19 11:23:05 +0800

    Couple of steps towards a simple Kart behaviour

--- Assets/Resources/Kart/Kart.cs
+++ Assets/Resources/Kart/Kart.cs
@@ -1,32 +1,98 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Kart : MonoBehaviour {
+public class Kart : MonoBehaviour
+{
+    public bool Bot = false;
 
-    public WheelCollider m_frontLeft;
-    public WheelCollider m_frontRight;
-    public WheelCollider m_backLeft;
-    public WheelCollider m_backRight;
+    public Transform FrontRightWheel;
+    public Transform FrontLeftWheel;
+    public Transform RearRightWheel;
+    public Transform RearLeftWheel;
 
-    private int m_speed = 150;
-    private int m_breaking = 1;
-    private int m_turning = 10;
+    public float Acceleration = 0.01f;
+    public float MaximumForwardVelocity = 1f;
+    public float MaximumBackwardVelocity = 1f;
+    public float WheelRotationFactor = 100f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(m_backRight.transform.position);
-        float verticalAxis = Input.GetAxis("Vertical") * m_speed;
-        float horizontalAxis = Input.GetAxis("Horizontal") * m_turning;
-        m_backLeft.motorTorque = m_backRight.motorTorque = verticalAxis;
-        m_frontLeft.steerAngle = m_frontRight.steerAngle = horizontalAxis;
+    private float _velocity = 0f;
 
-        if (Input.GetKey(KeyCode.Space)) {
-            m_backLeft.brakeTorque = m_backRight.brakeTorque = m_breaking;
+    private Transform[] _wheels = new Transform[4];
+
+    // Use this for initialization
+    void Start()
+    {
+        _wheels[0] = FrontRightWheel;
+        _wheels[1] = FrontLeftWheel;
+        _wheels[2] = RearRightWheel;
+        _wheels[3] = RearLeftWheel;
+    }
+
+    void FixedUpdate()
+    {
+        _velocity = _velocity + Acceleration * GetMovementMultiplier();
+
+        _velocity = ApplySurfaceFriction(_velocity);
+
+        Mathf.Clamp(_velocity, MaximumBackwardVelocity * -1, MaximumForwardVelocity);
+
+        transform.position = transform.position + transform.forward * _velocity;
+
+        RotateWheels();
+    }
+
+    private void RotateWheels()
+    {
+        foreach(Transform wheel in _wheels)
+        {
+            wheel.Rotate(wheel.right, _velocity * WheelRotationFactor);
         }
-	}
+    }
+
+    private int GetMovementMultiplier()
+    {
+        if(Bot)
+        {
+            //Bots always drive forward as there is no AI yet
+            return 1;
+        }
+        else
+        {
+            int movement = 0;
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                ++movement;
+            }
+
+            if (Input.GetKey(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                --movement;
+            }
+
+            return movement;
+        }
+    }
+
+    private float ApplySurfaceFriction(float VelocityBeforeFriction)
+    {
+        // This is a temporary measure until we can get the surface friction from the actual road
+        float accelerationDueToFriction = 0.001f;
+
+        if(VelocityBeforeFriction < 0f)
+        {
+            float VelocityAfterFriction = VelocityBeforeFriction + accelerationDueToFriction;
+            return Mathf.Clamp(VelocityAfterFriction, VelocityAfterFriction, 0f);
+        }
+        else if(VelocityBeforeFriction > 0f)
+        {
+            float VelocityAfterFriction = VelocityBeforeFriction - accelerationDueToFriction;
+            return Mathf.Clamp(VelocityAfterFriction, 0f, VelocityAfterFriction);
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+
 }
