af82ad11cf41bff29c445b0e74a9654ce73fd2d7
BobSmun <somemadeupname@gmail.com>
2015-04-21 01:20:04 +0800

    Hud

---
+++ Assets/Resources/UI/HUD.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+using UnityStandardAssets.Vehicles.Car;
+
+public static class RectTransformExtensions
+{
+    public static void SetDefaultScale(this RectTransform trans)
+    {
+        trans.localScale = new Vector3(1, 1, 1);
+    }
+    public static void SetPivotAndAnchors(this RectTransform trans, Vector2 aVec)
+    {
+        trans.pivot = aVec;
+        trans.anchorMin = aVec;
+        trans.anchorMax = aVec;
+    }
+
+    public static Vector2 GetSize(this RectTransform trans)
+    {
+        return trans.rect.size;
+    }
+    public static float GetWidth(this RectTransform trans)
+    {
+        return trans.rect.width;
+    }
+    public static float GetHeight(this RectTransform trans)
+    {
+        return trans.rect.height;
+    }
+
+    public static void SetPositionOfPivot(this RectTransform trans, Vector2 newPos)
+    {
+        trans.localPosition = new Vector3(newPos.x, newPos.y, trans.localPosition.z);
+    }
+
+    public static void SetLeftBottomPosition(this RectTransform trans, Vector2 newPos)
+    {
+        trans.localPosition = new Vector3(newPos.x + (trans.pivot.x * trans.rect.width), newPos.y + (trans.pivot.y * trans.rect.height), trans.localPosition.z);
+    }
+    public static void SetLeftTopPosition(this RectTransform trans, Vector2 newPos)
+    {
+        trans.localPosition = new Vector3(newPos.x + (trans.pivot.x * trans.rect.width), newPos.y - ((1f - trans.pivot.y) * trans.rect.height), trans.localPosition.z);
+    }
+    public static void SetRightBottomPosition(this RectTransform trans, Vector2 newPos)
+    {
+        trans.localPosition = new Vector3(newPos.x - ((1f - trans.pivot.x) * trans.rect.width), newPos.y + (trans.pivot.y * trans.rect.height), trans.localPosition.z);
+    }
+    public static void SetRightTopPosition(this RectTransform trans, Vector2 newPos)
+    {
+        trans.localPosition = new Vector3(newPos.x - ((1f - trans.pivot.x) * trans.rect.width), newPos.y - ((1f - trans.pivot.y) * trans.rect.height), trans.localPosition.z);
+    }
+
+    public static void SetSize(this RectTransform trans, Vector2 newSize)
+    {
+        Vector2 oldSize = trans.rect.size;
+        Vector2 deltaSize = newSize - oldSize;
+        trans.offsetMin = trans.offsetMin - new Vector2(deltaSize.x * trans.pivot.x, deltaSize.y * trans.pivot.y);
+        trans.offsetMax = trans.offsetMax + new Vector2(deltaSize.x * (1f - trans.pivot.x), deltaSize.y * (1f - trans.pivot.y));
+    }
+    public static void SetWidth(this RectTransform trans, float newSize)
+    {
+        SetSize(trans, new Vector2(newSize, trans.rect.size.y));
+    }
+    public static void SetHeight(this RectTransform trans, float newSize)
+    {
+        SetSize(trans, new Vector2(trans.rect.size.x, newSize));
+    }
+}
+
+public class HUD : MonoBehaviour
+{
+    public Transform HudPreFab;
+	// Use this for initialization
+	void Start ()
+    {
+        CarUserControl[] carControllers = FindObjectsOfType<CarUserControl>();
+
+        foreach(CarUserControl car in carControllers)
+        {
+            if(car.m_playerType == PlayerType.Drone)
+            {
+                continue;
+            }
+
+            CreateHud(car.m_playerType == PlayerType.Player1, car);
+        }
+	}
+
+    void CreateHud(bool Player1, CarUserControl car)
+    {
+        Transform Hud = (Transform)Instantiate(HudPreFab);
+        Hud.SetParent(this.gameObject.transform, false);
+
+        RectTransform pos = Hud.gameObject.GetComponent<RectTransform>();
+        RectTransformExtensions.SetPositionOfPivot(pos, new Vector2(0, Player1 ? Screen.height / 2 : 0));
+        RectTransformExtensions.SetHeight(pos, Screen.height / 2);
+
+        AmmoStorage storage = car.gameObject.GetComponentInChildren<AmmoStorage>();
+        Hud.GetComponentInChildren<AmmoDisplay>().SetAmmoStorage(storage);
+        Hud.GetComponentInChildren<ScoreDisplay>().SetAmmoStorage(storage);
+    }
+}
---
+++ Assets/Resources/UI/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    private AmmoStorage storage;
+    private UnityEngine.UI.Text t;
+
+    void Start()
+    {
+        t = GetComponent<UnityEngine.UI.Text>();
+    }
+
+    void Update()
+    {
+        t.text = storage.Score.ToString();
+    }
+
+    public void SetAmmoStorage(AmmoStorage kart)
+    {
+        storage = kart;
+    }
+}
