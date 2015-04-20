c64a1f103171d9fc17e6bb21a5b847818432df47
jstennant <jono.tennant@gmail.com>
2015-04-20 21:49:30 +0800

    aaa

--- Assets/Resources/Zombie/GreenShellZombie.cs
+++ Assets/Resources/Zombie/GreenShellZombie.cs
@@ -14,6 +14,18 @@ public class GreenShellZombie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + transform.forward * Speed;
+        RaycastHit hit;
+        if(Physics.Linecast(
+            transform.position,
+            transform.position + transform.forward * Speed,
+            out hit))
+        {
+            transform.position = hit.point;
+            Vector3 newForward = Vector3.Reflect(transform.forward, hit.normal);
+            newForward.y = 0f;
+            transform.forward = Vector3.Normalize(newForward);
+        }
+
+        transform.position += transform.forward * Speed;
     }
 }
